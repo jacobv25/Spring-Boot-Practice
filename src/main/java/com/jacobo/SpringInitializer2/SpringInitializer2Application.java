8625b20e8d3e13235903129f603b823b8def512d
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2022-03-13 13:16:37 -0800

    Initial commit

---
+++ src/main/java/com/jacobo/SpringInitializer2/SpringInitializer2Application.java
@@ -0,0 +1,13 @@
+package com.jacobo.SpringInitializer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobo/SpringInitializer2/SpringInitializer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobo.SpringInitializer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
